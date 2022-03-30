c4906edb5a61706f776f3d70d875ec7d6a5ff941
LimKiWoong <lkw4670@naver.com>
2022-03-30 20:13:05 +0900

    a

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # 임기웅
 
 [3월 30일]
+firebase 연동
 
 [3월 23일]
 $ git config --global user.name "LimKiWoong"
 $ git config --global user.email "lkw4670@naver.com"
 git config user.name
 git config user.email
+extenstions에서 git graph 설치
+VSC에서 Git 연동
+
+[npm start] -> 홈페이지실행
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmVzAR6PxB6P7ONg_iNqq3IU0F3M6onyY",
+  authDomain: "nwitter-dd2e4.firebaseapp.com",
+  projectId: "nwitter-dd2e4",
+  storageBucket: "nwitter-dd2e4.appspot.com",
+  messagingSenderId: "224438348197",
+  appId: "1:224438348197:web:4e76f25000697bf6b0204c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
